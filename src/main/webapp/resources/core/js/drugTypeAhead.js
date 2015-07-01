994cd63f7d57a2a21f25dfe0b46f2ddf33bc0a05
krishna.chaganti@gmail.com <kvchaganti@gmail.com>
2015-07-01 18:55:31 -0400

    fixes #15 fixed centering char title fixes #6 removed count fixes #1 fixed typeahead fixes #2 fixes #3

--- src/main/webapp/resources/core/js/drugTypeAhead.js
+++ src/main/webapp/resources/core/js/drugTypeAhead.js
@@ -1,39 +1,45 @@
-var baseSearchURL = "https://api.fda.gov/drug/label.json?";
-var limit = 100;
+var baseSearchURL = "https://api.fda.gov/drug/event.json?count=patient.drug.medicinalproduct.exact&limit=1000";
+var limit = 1000;
 var openFDAKey = "uN2mlhVgIcwgvh8FrfmpT5f7U65RGxnrtrZCxInc"; /* API key for openFDA queries */
 
 var searchURL = baseSearchURL;
 searchURL += ("api_key=" + openFDAKey);
 searchURL += ("&limit=" + limit);
-searchURL += "&search=brand_name:";
 
-var drugs = new Bloodhound({
-    datumTokenizer: function (datum) {
-        return Bloodhound.tokenizers.whitespace(datum.value);
-    },
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: {
-        url: searchURL + "%QUERY",
-        filter: function (drugs) {
-            // Map the remote source JSON array to a JavaScript object array
-            return $.map(drugs.results, function (drug) {
-                return {
-                    value: drug.openfda.brand_name
-                };
-            });
-        },
-        wildcard: "%QUERY"
-    }
+var drugList=[];
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+    // an array that will be populated with substring matches
+    matches = [];
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+    cb(matches);
+  };
+};
+
+$.getJSON(searchURL, {})
+.done(function(data) {
+      $.each(data.results, function(i, result){
+        drugList.push(result.term);
+      });
+      initiateTypeAhead();
 });
 
- 
 function initiateTypeAhead(){
-  $('#search_drug').typeahead({
-      hint: false,
-      minLength: 2 }, {
-      displayKey: 'value',
-      source: drugs.ttAdapter(),
-  });
-// Initialize the Bloodhound suggestion engine
-drugs.initialize();
+  var ta = $('#search_drug').typeahead({
+              hint: false,
+              minLength: 1},
+              {
+              name: "drugs",
+              source: substringMatcher(drugList)
+          })
 }
